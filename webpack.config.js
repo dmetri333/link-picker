fce0827b052e4d5b8286404cecb4511ac2aeeb80
Demetri <dmetri333@gmail.com>
2020-04-28 16:07:56 -0400

    Updated link picker output

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'linkPicker.js',
+    filename: 'link-picker.js',
     libraryTarget: 'var',
     library: 'LinkPicker'
   }
