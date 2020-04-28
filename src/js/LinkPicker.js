0a76a95098cd32d13b07e15ea3d1f2b5062f83b2
Demetri <dmetri333@gmail.com>
2020-04-28 17:10:14 -0400

    Fixed id call

--- src/js/LinkPicker.js
+++ src/js/LinkPicker.js
@@ -102,7 +102,7 @@ class LinkPicker {
 		
 		let html = '';
 		$('[data-structure=container]').each(function(index, item) {
-			html += Util.supplant(this.options.templates.existingContentItem, { text: item.data('id'), url: '#'+item.data('id') });
+			html += Util.supplant(this.options.templates.existingContentItem, { text: item.id, url: '#'+item.id });
 		}.bind(this));
 
 		this.sectionList.html(html);
